69c365c8f1102d4205acd0e904b6edef63acc92c
Eric Le Donge <eric.ledonge@gmail.com>
2025-10-25 22:23:38 -0400

    :bug: fix betterAuth client

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -4,10 +4,12 @@ import { nextCookies } from "better-auth/next-js";
 import { admin } from "better-auth/plugins";
 
 import { db } from "@/lib/db";
+import * as schema from "@/lib/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema,
   }),
   emailAndPassword: {
     enabled: true,
