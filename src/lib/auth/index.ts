e80f53a1ce40f2a98bbe5003edce9a3a0beaf49c
Eric Le Donge <eric.ledonge@gmail.com>
2025-10-26 22:24:13 -0400

    test

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -7,10 +7,6 @@ import { db } from "@/lib/db";
 import * as schema from "@/lib/db/schema";
 
 export const auth = betterAuth({
-  baseURL:
-    process.env.BETTER_AUTH_URL || process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000",
   secret: process.env.BETTER_AUTH_SECRET,
   database: drizzleAdapter(db, {
     provider: "pg",
