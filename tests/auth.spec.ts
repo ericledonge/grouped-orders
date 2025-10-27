b436999976e9f91dfab6848b7d9ab3d1231b66f1
Eric Le Donge <eric.ledonge@gmail.com>
2025-10-26 21:36:29 -0400

    test

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -10,6 +10,8 @@ test("user can sign up", async ({ page }) => {
   await page.getByLabel("Password").fill("password123");
   await page.getByRole("button", { name: "Create an account" }).click();
 
+  await page.waitForURL("**/");
+
   await expect(page.getByText("Bienvenue")).toBeVisible();
 });
 
@@ -19,6 +21,8 @@ test("user can sign in", async ({ page }) => {
   await page.getByRole("textbox", { name: "Email" }).fill("test@example.com");
   await page.getByLabel("Password").fill("password123");
   await page.getByRole("button", { name: "Login" }).click();
+
+  await page.waitForURL("**/");
 
   await expect(page.getByText("Bienvenue")).toBeVisible();
 });
